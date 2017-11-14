258b2580ab20958ed032cb902eef0c5d654bd83e
yshalsager <ysh-alsager@hotmail.com>
2017-11-14 15:44:41 +0200

    23/4/2017

--- normandy/vendorsetup.sh
+++ normandy/vendorsetup.sh
@@ -1,12 +1,12 @@
 /bin/cp device/nokia/normandy/post_process_props_hook.py build/tools/
 chmod  0775 build/tools/post_process_props_hook.py
 
-add_lunch_combo cm_normandy-eng
-add_lunch_combo cm_normandy-userdebug
-add_lunch_combo cm_normandy-user dist
+add_lunch_combo lineage_normandy-eng
+add_lunch_combo lineage_normandy-userdebug
+add_lunch_combo lineage_normandy-user dist
 
-sh device/nokia/normandy/patches/apply.sh
-croot
+#sh device/nokia/normandy/patches/apply.sh
+#croot
 
 rm -rf out/target/product/normandy/obj/PACKAGING/target_files_intermediates
 rm -f out/target/product/normandy/system/build.prop
